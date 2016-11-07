ad4a25402174e6bee013a6fe39628650bcd1346d
Eric Higdon <ehigdon@answersingenesis.org>
2016-11-07 08:27:39 -0500

    Added update function and resize images to be 1920px wide

--- display.py
+++ display.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageTk
 from io import BytesIO
+from threading import Thread
 import tkinter as tk
 import time
 import os
@@ -8,16 +9,15 @@ import requests
 class Application(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self,parent)
-        #root.attributes("-fullscreen", True)
         root.overrideredirect(True)
         root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
         root.focus_set()  # <-- move focus to this widget
         root.bind("<Escape>", lambda e: e.widget.quit())
         self.pack(fill=tk.BOTH)
         root.protocol("WM_DELETE_WINDOW", self.close)
-        url = 'http://accordapp.com/display/slides'
-        headers = {'AUTHORIZATION': 'Basic ZndiYzpMMHYzVzBya3M='}
-        response = requests.get(url, headers=headers)
+        self.url = 'http://accordapp.com/display/slides'
+        self.headers = {'AUTHORIZATION': 'Basic ZndiYzpMMHYzVzBya3M='}
+        response = requests.get(self.url, headers=self.headers)
         self.photos = [
             slide['image'] for slide in response.json()
         ] 
@@ -28,11 +28,16 @@ class Application(tk.Frame):
     def create_widgets(self):
         self.label = tk.Label(self, bd=0)
         self.label.pack()
-        self.animate()
+        Thread(target=self.animate).start()
+        Thread(target=self.update).start()
 
     def animate(self):
         img_response = requests.get(self.photos[self.index].replace('https', 'http'))
         img = Image.open(BytesIO(img_response.content))
+        basewidth = 1920
+        wpercent = (basewidth/float(img.size[0]))
+        hsize = int((float(img.size[1])*float(wpercent)))
+        img = img.resize((basewidth,hsize), Image.ANTIALIAS)
         img_obj = ImageTk.PhotoImage(img)
         self.label.config(image=img_obj)
         self.label.image=img_obj
@@ -40,7 +45,16 @@ class Application(tk.Frame):
             self.index = 0
         else:
             self.index += 1
-        self.after(10000, self.animate)
+        self.after(1000, self.animate)
+
+    def update(self):
+        response = requests.get(self.url, headers=self.headers)
+        self.photos = [
+            slide['image'] for slide in response.json()
+        ] 
+        print(self.photos)
+        self.index = 0
+        self.after(600000, self.update)
 
     def close(self):
         os._exit(0)
