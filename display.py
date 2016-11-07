a0f4f098fa3611af2945c103634e24dfbed3ed8d
Eric Higdon <ehigdon@answersingenesis.org>
2016-11-07 08:54:15 -0500

    Added animation

--- display.py
+++ display.py
@@ -15,46 +15,68 @@ class Application(tk.Frame):
         root.bind("<Escape>", lambda e: e.widget.quit())
         self.pack(fill=tk.BOTH)
         root.protocol("WM_DELETE_WINDOW", self.close)
-        self.url = 'http://accordapp.com/display/slides'
-        self.headers = {'AUTHORIZATION': 'Basic ZndiYzpMMHYzVzBya3M='}
-        response = requests.get(self.url, headers=self.headers)
-        self.photos = [
-            slide['image'] for slide in response.json()
-        ] 
         self.index = 0
+        self.ready = False
+        logo = Image.open('slide1.jpg')
+        self.logo_obj = ImageTk.PhotoImage(logo)
+        self.photos = []
 
         self.create_widgets()
         
     def create_widgets(self):
         self.label = tk.Label(self, bd=0)
         self.label.pack()
+        Thread(target=self.get_slides).start()
         Thread(target=self.animate).start()
-        Thread(target=self.update).start()
 
     def animate(self):
-        img_response = requests.get(self.photos[self.index].replace('https', 'http'))
-        img = Image.open(BytesIO(img_response.content))
-        basewidth = 1920
-        wpercent = (basewidth/float(img.size[0]))
-        hsize = int((float(img.size[1])*float(wpercent)))
-        img = img.resize((basewidth,hsize), Image.ANTIALIAS)
-        img_obj = ImageTk.PhotoImage(img)
-        self.label.config(image=img_obj)
-        self.label.image=img_obj
-        if self.index >= len(self.photos)-1:
-            self.index = 0
+        if self.ready:
+            self.label.config(image=self.photos[self.index]['image'])
+            self.label.image=self.photos[self.index]['image']
+            if self.index >= len(self.photos)-1:
+                self.index = 0
+            else:
+                self.index += 1
+            if self.photos[self.index]['wait']:
+                self.after(10000, self.animate)
+            else:
+                self.after(10, self.animate)
         else:
-            self.index += 1
-        self.after(1000, self.animate)
+            self.label.config(image=self.logo_obj)
+            self.label.image=self.logo_obj
+            self.after(5000, self.animate)
 
-    def update(self):
-        response = requests.get(self.url, headers=self.headers)
-        self.photos = [
-            slide['image'] for slide in response.json()
-        ] 
-        print(self.photos)
-        self.index = 0
-        self.after(600000, self.update)
+    def get_slides(self):
+        url = 'http://accordapp.com/display/slides'
+        headers = {'AUTHORIZATION': 'Basic ZndiYzpMMHYzVzBya3M='}
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        anim_index = 0
+        while anim_index < len(data):
+            img1 = self.get_img(data[anim_index]['image'])
+            anim_index += 1
+            try:
+                img2 = self.get_img(data[anim_index]['image'])
+            except IndexError:
+                img2 = self.get_img(data[0]['image'])
+            alpha = 0
+            while alpha <= 1:
+                img = Image.blend(img1, img2, alpha)
+                img_obj = ImageTk.PhotoImage(img)
+                if alpha == 0:
+                    original = True
+                else:
+                    original = False
+                self.photos.append({'image': img_obj, 'wait': original})
+                alpha += .1
+        self.ready = True
+        self.after(3000, self.get_slides)
+
+    def get_img(self, url):
+        img_response = requests.get(url.replace('https', 'http'))
+        img = Image.open(BytesIO(img_response.content))
+        img = img.resize((1920, 1080))
+        return img
 
     def close(self):
         os._exit(0)
