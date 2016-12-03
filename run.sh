e8fa264d6f65cd146c17728bec455370fe68b52b
Eric Higdon <eric.s.higdon@gmail.com>
2016-12-03 17:21:06 +0000

    updated run.sh file

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 # run.sh
 # navigate to this directory, then execute python script
 
-cd ~/accord-display
-workon display
-sudo python display.py
+PATH=/home/pi/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
+export DISPLAY=:0.0
+cd /home/pi/accord-display
+sudo /home/pi/Envs/display/bin/python /home/pi/accord-display/display.py
 
